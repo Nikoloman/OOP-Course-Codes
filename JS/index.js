4a49470bd07fa40eed6005d56a9de8e155fec20e
Nikoloman <hectorvel52@gmail.com>
2023-01-05 14:24:10 -0600

    Printing objects and it's properties in JS

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,7 @@ var car = new Car("ABC-123", new Account("Oscar Cabrales", "OSC123"));
 car.id = 1;
 car.passenger = 4;
 car.printDataOfCar();
+
+var uberX = new UberX("DEF-456", new Account("Maximiliano Ledesma", "MAX123"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataOfCar();
