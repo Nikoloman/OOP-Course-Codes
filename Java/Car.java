b1b345d22d455fb7ed4256aa13fed433b78ae66e
Nikoloman <hectorvel52@gmail.com>
2023-01-06 13:46:40 -0600

    Creating setters and getters in Java

--- Java/Car.java
+++ Java/Car.java
@@ -6,7 +6,7 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passengers;
+    private Integer passengers;
 
     //Constructor
     public Car(String license, Account driver){
@@ -20,5 +20,15 @@ public class Car {
         System.out.println("The driver's name is: " + driver.name);
         System.out.println("The car's license is: " + license);
         System.out.println("The car has a capacity of " + passengers + " passengers");
+    }
+
+    //Getter
+    public Integer getPassengers(){
+        return passengers;
+    }
+
+    //Setter
+    public void setPassengers(Integer passengers){
+        this.passengers = passengers;
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -7,27 +7,27 @@ class Main{
 
         Car car = new Car("ABC-123", new Account("Oscar Cabrales","OSC123"));
         car.id = 1;
-        car.passengers = 4;
+        car.setPassengers(4);
 
         System.out.println("The ID of the car is: " + car.id);
         System.out.println("The driver's name is: " + car.driver.name);
         System.out.println("The car's license is: " + car.license);
-        System.out.println("The car has a capacity of " + car.passengers + " passengers");
+        System.out.println("The car has a capacity of " + car.getPassengers() + " passengers");
         System.out.println();
 
         Car car2 = new Car("DEF-456", new Account("Max Ledesma", "MAX456"));
         car2.id = 2;
-        car2.passengers = 5;
+        car2.setPassengers(5);
 
         System.out.println("The ID of the car is: " + car2.id);
         System.out.println("The driver's name is: " + car2.driver.name);
         System.out.println("The car's license is: " + car2.license);
-        System.out.println("The car has a capacity of " + car2.passengers + " passengers");
+        System.out.println("The car has a capacity of " + car2.getPassengers() + " passengers");
         System.out.println();
 
         Car car3 = new Car("GHI-789", new Account("Michel Ortiz", "MIC789"));
         car3.id = 3;
-        car3.passengers = 2;
+        car3.setPassengers(2);
         car3.printDataOfCar();
 
     }
